3724e0147e75d0a862cccd7305d6d55721ca5806
ks31030 <143372209+ks31030@users.noreply.github.com>
2024-01-02 14:53:03 +0900

    ff

--- spring-react/client/src/AddProduct.js
+++ spring-react/client/src/AddProduct.js
@@ -1,25 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
-import 
-//reace-router-dom
-function App() {
-    //data가 아닌 products로 적을 예정
-    const [products, setProducts] = useState([]);
+
+function AddProduct({onAddProduct}) {
     const [newProduct, setNewProduct] = useState({name: '', price: 0});
 
-    //get axios api
-    useEffect(() => {
-    const fetchData = async () => {
-        try {
-            await axios.get(
-                'http://localhost:8081/api/item'
-            )
-        } catch(error) {
-            console.log("데이터가 잘못되었습니다.", error);
-        }
-    };
-    fetchData();
-}, []);
+    // //get axios api
+    // useEffect(() => {
+    // const fetchData = async () => {
+    //     try {
+    //         await axios.get(
+    //             'http://localhost:8081/api/item'
+    //         )
+    //     } catch(error) {
+    //         console.log("데이터가 잘못되었습니다.", error);
+    //     }
+    // };
+    // fetchData();
+    // }, []);
 
     //post axios api
     const handleInputChange = (e) => {
@@ -35,23 +32,16 @@ function App() {
                 newProduct, {withCredentials: true}
             )
             //변경된 데이터값 저장
-            setProducts((prevProduct) => [...prevProduct, response.data]);
+            onAddProduct((prevProduct) => [...prevProduct, response.data]);
             //저장 후 값 초기화
             setNewProduct({name: '', price: 0});
         } catch(error) {
             console.log('error', error);
         }
-    }
+    };
+
     return (
         <div>
-            <h1>상품 리스트</h1>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>
-                        {product.name} - $ {product.price}
-                    </li>
-                ))}
-            </ul>
             <h2>상품 추가</h2>
             <div>
                 <label>상품명 : </label>
@@ -65,5 +55,4 @@ function App() {
         </div>
     )
 }
-
-export default App;
+export default AddProduct;
--- spring-react/client/src/App.js
+++ spring-react/client/src/App.js
@@ -1,89 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+//reace-router-dom
 function App() {
-  const [data, setData] = useState([]);
-  const [newUser, setNewUser] = useState({userName:'', email:''});
-  //fetchData라는 이름을 정의해서 try-catch문을 사용해 비동기 작업 중
-  //발생하는 에러를 잡아내고 콘솔에 메시지를 출력하는 것.
-  //간접적으로 호출.
+    //data가 아닌 products로 적을 예정
+    const [products, setProducts] = useState([]);
+    const [newProduct, setNewProduct] = useState({name: '', price: 0});
 
-  //useEffect 안에서 직접 비동기 작업을 수행.
-  //간단하게 catch문을 사용해서 에러를 처리하고 콘솔에 에러 메시지를 출력.
-  useEffect(() => {
+    //get axios api
+    useEffect(() => {
     const fetchData = async () => {
-      try {
-        const res = await axios.get('http://localhost:8080/api/user', {
-          withCredentials:true,
-        });
-        setData(res.data);
-      } catch (error) {
-        console.log(error);
-      }
+        try {
+            await axios.get(
+                'http://localhost:8081/api/item'
+            )
+        } catch(error) {
+            console.log("데이터가 잘못되었습니다.", error);
+        }
     };
     fetchData();
-  }, []);
-  
-  //데이터 작성한 내용으로 변경하는 함수
-  const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setNewUser((prevUser) => ({...prevUser, [name] : value}));
-  };
+    }, []);
 
-  //전송하는 Button 함수 추가
-  //데이터 보내줄 Post 추가
-  const handleAddUser = async () => {
-    try {
-      const response = await axios.post(
-        'http://localhost:8080/api/user', newUser, {withCredentials:true}
-      );
-      //변경된 데이터 값
-      setData((prevData) => [...prevData, response.data]);
-      //데이터 저장되고 빈 값으로 초기화 원하면 진행
-      setNewUser({userName:'', email:''});
-    } catch (error) {
-      console.error('데이터가 부적합합니다.', error)
-    }
-  };
+    //post axios api
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setNewProduct((prevProduct) => ({...prevProduct, [name]: value}));
+    };
 
-  useEffect(() => {
-    axios
-    .get('http://localhost:8080/api/user', {withCredentials:true,})
-    //response = res 같은 의미이며, 안에 변수값은 정해진 변수값은 없지만 되도록이면 res response를 사용하면 좋음.
-    .then((res) => {
-      setData(res.data);
-    })
-    .catch((error) => {
-      console.log('데이터없음', error);
-    });
-  }, []);
+    const handleAddProduct = async () => {
+        try{
+            //input값 보내 줄 post 작성
+            const response = await axios.post(
+                'http://localhost:8081/api/item',
+                newProduct, {withCredentials: true}
+            )
+            //변경된 데이터값 저장
+            setProducts((prevProduct) => [...prevProduct, response.data]);
+            //저장 후 값 초기화
+            setNewProduct({name: '', price: 0});
+        } catch(error) {
+            console.log('error', error);
+        }
+    };
 
-  return (
-    <div>
-      <h1>API 호출 확인</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>
-            이름 : {user.userName}
-            <br/>
-            이메일 : {user.email}
-          </li>
-        ))}
-      </ul>
-      <h2>새로운 유저 저장</h2>
-      <div>
-        <label>회원 이름 : </label>
-        <input type='text' name='userName' value={newUser.userName} onChange={handleInputChange}/>
-      </div>
-      <div>
-        <label>회원 이메일 : </label>
-        <input type='email' name='email' value={newUser.email} onChange={handleInputChange}/>
-      </div>
-      <button onClick={handleAddUser}>유저 저장</button>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <h1>상품 리스트</h1>
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>
+                            {product.name} - $ {product.price}
+                        </li>
+                    ))}
+                </ul>
+                <h2>상품 추가</h2>
+                <div>
+                    <label>상품명 : </label>
+                    <input type="text" name="name" value={newProduct.name} onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label>r가 격 : </label>
+                    <input type="number" name="price" value={newProduct.name} onChange={handleInputChange}/>
+                </div>
+                <button onClick={handleAddProduct}>상품 추가</button>
+            </div>
+            <Routes>
+                <Route path="add">
+                    <h2>상품리스트 Js 만들어서 element 추가 예정</h2>
+                </Route>
+                <Route path="/item">
+                    <h2>상품 리스트</h2>
+                </Route>
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
